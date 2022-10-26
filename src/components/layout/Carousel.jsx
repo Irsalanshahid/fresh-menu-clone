f3b7bb1e1d76f080bf47ed4bc62f17bf1b26cffc
Arslan Shahid <irsalan.uetian08@gmail.com>
2022-10-26 11:46:55 +0500

    In progress

--- src/components/layout/Carousel.jsx
+++ src/components/layout/Carousel.jsx
@@ -1,31 +1,62 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState,useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLessThan, faGreaterThan } from "@fortawesome/free-solid-svg-icons";
 
+const delay = 2500;
+const imgLinks = ["https://s3-ap-southeast-1.amazonaws.com/foodvista.1/469ff800-2800-4932-a0c4-7244c157e090.jpg",
+"https://s3-ap-southeast-1.amazonaws.com/foodvista.1/3cc0d71e-0ef6-412e-8a35-7d9f2a18fdd6.jpg",
+"https://s3-ap-southeast-1.amazonaws.com/foodvista.1/3c2a7e34-1f2c-43e2-97ec-d01ddf472c21.jpg",
+"https://s3-ap-southeast-1.amazonaws.com/foodvista.1/4428631b-38be-4e91-ad24-44657c7f9e2b.jpg",
+"https://s3-ap-southeast-1.amazonaws.com/foodvista.1/adbe24f0-7291-4e9e-ad1c-2ae3d649f984.jpg",
+"https://s3-ap-southeast-1.amazonaws.com/foodvista.1/24ae0ab9-9914-4529-bb17-5aee99b58641.jpg",
+"https://s3-ap-southeast-1.amazonaws.com/foodvista.1/ca00a605-cbe2-4455-aed1-37d631a23254.jpeg"
+]
+
 const Carousel = () => {
   const clickPrev = () => {
-    const Carousel = document.getElementById("carousel");
-    let width = 0;
-    width = Carousel.clientWidth;
-    Carousel.scrollLeft = Carousel.scrollLeft - width / 3;
-    console.log(Carousel.scrollLeft - width / 3);
+    // const Carousel = document.getElementById("carousel");
+    // let width = 0;
+    // width = Carousel.clientWidth;
+    // Carousel.scrollLeft = Carousel.scrollLeft - width / 3;
+    // console.log(Carousel.scrollLeft - width / 3);
+    setIndex((prevIndex) => (
+      prevIndex > 0 ? prevIndex - 1 : prevIndex
+      
+    ))
   };
   const clickNext = () => {
-    const Carousel = document.getElementById("carousel");
-    let width = 0;
-    width = Carousel.clientWidth;
-    Carousel.scrollLeft = Carousel.scrollLeft + width / 3;
-    console.log(Carousel.scrollLeft + width / 3);
+    setIndex((prevIndex) => (
+      prevIndex < Math.round((imgLinks.length/1.5)-1.5) ? prevIndex + 1 : prevIndex
+      
+    ))
   };
+  const [index, setIndex] = useState(0);
+
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
   useEffect(() => {
-    setTimeout(() => {
-      const Carousel = document.getElementById("carousel");
-      let width = 0;
-      width = Carousel.clientWidth;
-      Carousel.scrollLeft = Carousel.scrollLeft + width / 3;
-      console.log(Carousel.scrollLeft + width / 3);
-    }, 20);
-  },[]);
+    
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === Math.round((imgLinks.length/1.5)-1.5)? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+
   return (
     <section className="relative  flex h-auto w-full overflow-x-hidden">
       <div className="w-max-20 absolute z-10 flex h-full w-20 items-center justify-center bg-transparent ">
@@ -49,42 +80,20 @@ const Carousel = () => {
       </div>
 
       <div
-        className="flex gap-x-4 overflow-x-hidden scroll-smooth"
+        className="flex gap-x-4 scroll-smooth transition-transform ease-in-out"
         id="carousel"
+        style={{ transform: `translate3d(${-index * 500}px, 0, 0)` }}
       >
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/469ff800-2800-4932-a0c4-7244c157e090.jpg"
-          className="h-auto w-[401px]"
-        ></img>
+        {imgLinks.map((val,idx) => <img
+          src={val}
+          className="h-auto w-[401px] cursor-pointer"
+          key={idx}
+        ></img>)}
+        
 
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/3cc0d71e-0ef6-412e-8a35-7d9f2a18fdd6.jpg"
-          className="h-auto w-[401px]"
-        ></img>
+        
 
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/3c2a7e34-1f2c-43e2-97ec-d01ddf472c21.jpg"
-          className="h-auto w-[401px]"
-        ></img>
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/4428631b-38be-4e91-ad24-44657c7f9e2b.jpg"
-          className="h-auto w-[401px]"
-        ></img>
-
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/adbe24f0-7291-4e9e-ad1c-2ae3d649f984.jpg"
-          className="h-auto w-[401px]"
-        ></img>
-
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/24ae0ab9-9914-4529-bb17-5aee99b58641.jpg"
-          className="h-auto w-[401px]"
-        ></img>
-
-        <img
-          src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/ca00a605-cbe2-4455-aed1-37d631a23254.jpeg"
-          className="h-auto w-[401px]"
-        ></img>
+        
       </div>
     </section>
   );
